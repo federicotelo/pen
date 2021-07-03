e347a0615af9847a036cab40470713d15ec6919d
Marcelo Pagni <marcelo.pagni@gmail.com>
2021-07-03 02:32:17 -0300

    Fixing del titulo con logo

--- src/Titulo.js
+++ src/Titulo.js
@@ -3,7 +3,8 @@ import React from 'react';
 
 const Titulo = () => {
    return (  
-      <div className="content text-center bg-primary">   
+      <div className="content text-center bg-primary">
+         <img alt="pen-logo" src="./assets/pen-100.png" width="100"/>
          <h1>PEN Global Observatory for Freedom of Expression</h1>
          <h3>Since 1921 guardians of freedom of expression around the world.</h3>
       </div>
